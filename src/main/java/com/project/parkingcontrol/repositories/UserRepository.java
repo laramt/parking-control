a2e47b5e9d00a2976a5c8e2f2d883a25f8caf702
Lara Menck <lara.menckt@gmail.com>
2022-12-07 16:56:15 -0300

    database authentication

---
+++ src/main/java/com/project/parkingcontrol/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.project.parkingcontrol.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.parkingcontrol.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>{
+
+	Optional<User> findByUsername(String username);
+	
+}
